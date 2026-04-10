a1873b70b7151e4c8cc92cf466124e3d194329bc
00aaryan00 <aryan28nov05@gmail.com>
2026-04-10 17:29:47 +0530

    fix crash

--- app.js
+++ app.js
@@ -17,18 +17,17 @@ const MONGODB_URL = process.env.MONGODB_URL || "mongodb://localhost:27017/short-
 
 app.set("trust proxy", 1);
 app.set("view engine", "ejs");
-app.set("views", path.resolve("./views"));
+app.set("views", path.join(__dirname, "views"));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(checkAuth);
 
+app.get("/url/:shortId", asyncHandler(handleRedirectShortURL));
 app.use("/url", restrictToLoggedinUserOnly, urlRoute);
 app.use("/user", userRoute);
 app.use("/", staticRoute);
-
-app.get("/url/:shortId", asyncHandler(handleRedirectShortURL));
 
 app.use((req, res) => {
   res.status(404).render("notFound", {
